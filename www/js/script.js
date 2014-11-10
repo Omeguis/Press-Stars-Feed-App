5678598c108e7aae0c4bac9970591d1fc752eb50
Omeguis <cmluciani@hotmail.com>
2014-11-10 15:51:02 -0300

    updates 2

---
+++ www/js/script.js
@@ -0,0 +1,74 @@
+function listPosts (data) {
+	var output = '<form class="ui-filterable"><input id="searchposts" data-type="search"></form>';
+
+	output += '<ul data-role="listview" data-filter="true" data-input="#searchposts">';
+	$.each(data.posts, function(key, val)  {
+
+		var tempDiv = document.createElement("tempDiv");
+		tempDiv.innetHTML = val.excerpt;
+		$("a" , tempDiv).remove();
+		var excerpt = tempDiv.innerHTML;
+
+		output += '<li>';
+		output += '<a href="#blogpost" onclick = "showPost (' + val.id + ')">';
+		output += '<h3>' + val.title + "</h3>";
+		output += '<p>' + val.title + "</p>";
+		output += '</a>';
+		output += '</li>';
+	}); 
+
+	output += "</ul>";
+	$('#postlist').html(output);
+
+} //listPosts
+
+
+
+function listPods (data) {
+	var output = '<form class="ui-filterable"><input id="searchposts" data-type="search"></form>';
+
+	output += '<ul data-role="listview" data-filter="true" data-input="#searchposts">';
+	$.each(data.posts, function(key, val)  {
+
+		var tempDiv = document.createElement("tempDiv");
+		tempDiv.innetHTML = val.excerpt;
+		$("a" , tempDiv).remove();
+		var excerpt = tempDiv.innerHTML;
+
+		output += '<li>';
+		output += '<a href="#blogpod" onclick = "showPod (' + val.id + ')">';
+		output += '<h3>' + val.title + "</h3>";
+		output += '<p>' + val.title + "</p>";
+		output += '</a>';
+		output += '</li>';
+	}); 
+
+	output += "</ul>";
+	$('#podlist').html(output);
+
+} //listPosts
+
+
+
+function showPost(id) {
+	$.getJSON('http://press-stars.com/?json=get_post&post_id=' + id + '&callback=?',
+	function(data) {
+		var output = '<h3>' + data.post.title + '</h3>';
+		output += data.post.content;
+		$('#mypost').html(output);
+
+	});
+
+}
+
+
+function showPod(id) {
+	$.getJSON('http://press-stars.com/?json=get_post&post_id=' + id + '&callback=?',
+	function(data) {
+		var output = '<h3>' + data.post.title + '</h3>';
+		output += data.post.content;
+		$('#mypod').html(output);
+
+	});
+
+}
